d870de164cbc3af61804e8f14e5b693e9cc4824e
Alexandre Pilkiewicz <alexandre.pilkiewicz@polytechnique.org>
2011-09-27 18:14:23 +0200

    -

---
+++ test.v
@@ -0,0 +1,13 @@
+Definition foo coin pan :=
+  blah.coin.
+Definition foo coin pan :=
+  blah coin.
+Lemma blah: coin.
+Proof.
+sqdfkjh sqdlfkj mlkj/. mlqskdjf . msqldkjf.
+mlqsdkfj .
+Qed.
+
+Notation qmsldfjk .
+
+Definition hop :foo.
